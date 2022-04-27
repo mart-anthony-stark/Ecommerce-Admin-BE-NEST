856aa2367bb9dae8713d5416fd6187849198979d
mart-anthony-stark <martsalazar0@gmail.com>
2022-04-27 22:32:25 +0800

    Add mongoose and config modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    AuthModule,
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   controllers: [],
   providers: [],
 })
